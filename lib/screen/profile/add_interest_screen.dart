6a88745a751271417ef1922ac5e34284f6277cf7
Wai Phyo Aung <waiphyo.aung@yomabank.com>
2024-04-24 21:29:48 +0700

    ui fix for add interest screen

--- lib/screen/profile/add_interest_screen.dart
+++ lib/screen/profile/add_interest_screen.dart
@@ -42,155 +42,153 @@ class AddInterestState extends BaseScreenState<AddInterestScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            const BgView(),
-            Container(
-              margin: const EdgeInsets.only(top: 40),
-              child: ListTile(
-                leading: InkWell(
-                  onTap: (){
-                    context.back();
-                  },
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.chevron_left, color: Colors.white,),
-                      SizedBox(width: 5,),
-                      Text("Back", style: TextStyle(color: Colors.white, fontSize: 16),)
-                    ],
-                  ),
+      body: Stack(
+        children: [
+          const BgView(),
+          Container(
+            margin: const EdgeInsets.only(top: 40),
+            child: ListTile(
+              leading: InkWell(
+                onTap: (){
+                  context.back();
+                },
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.chevron_left, color: Colors.white,),
+                    SizedBox(width: 5,),
+                    Text("Back", style: TextStyle(color: Colors.white, fontSize: 16),)
+                  ],
                 ),
-                trailing: TextButton(
-                    onPressed: formSubmit,
-                    child: const Padding(
-                      padding: EdgeInsets.only(right: 15, left: 15),
-                      child: Text("Save", style: TextStyle(
-                        fontSize: 16, color: Color(0xFFFFE2BE)),),
+              ),
+              trailing: TextButton(
+                  onPressed: formSubmit,
+                  child: const Padding(
+                    padding: EdgeInsets.only(right: 15, left: 15),
+                    child: Text("Save", style: TextStyle(
+                      fontSize: 16, color: Color(0xFFFFE2BE)),),
+                  )
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 120),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 30,),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text("Tell everyone about yourself", style: TextStyle(fontSize: 16, color: Color(0xFFFFE2BE)),)),
+                    const SizedBox(height: 10,),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text("What interest you?", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
+                    ),
+                    const SizedBox(height: 30,),
+                    TextFieldTags<String>(
+                      textfieldTagsController: _stringTagController,
+                      initialTags: controller.localProfile.value.getDefault().interests,
+                      textSeparators: const [' ', ','],
+                      letterCase: LetterCase.normal,
+                      validator: (String tag) {
+                        if (_stringTagController.getTags!.contains(tag)) {
+                          return 'You\'ve already entered that';
+                        }
+                        return null;
+                      },
+                      inputFieldBuilder: (context, inputFieldValues) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 15),
+                          child: TextField(
+                            onTap: () {
+                              _stringTagController.getFocusNode?.requestFocus();
+                            },
+                            controller: inputFieldValues.textEditingController,
+                            focusNode: inputFieldValues.focusNode,
+                            decoration: InputDecoration(
+                              isDense: true,
+                              border: const OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(7))
+                              ),
+                              focusedBorder: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(7)),
+                                borderSide: BorderSide(
+                                  color: Color(0xFE9E6E6)
+                                )
+                              ),
+                              errorText: inputFieldValues.error,
+                              prefixIconConstraints:
+                              BoxConstraints(maxWidth: _distanceToField * 0.8),
+                              prefixIcon: inputFieldValues.tags.isNotEmpty
+                                  ? SingleChildScrollView(
+                                controller: inputFieldValues.tagScrollController,
+                                scrollDirection: Axis.vertical,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Wrap(
+                                      runSpacing: 4.0,
+                                      spacing: 4.0,
+                                      children:
+                                      inputFieldValues.tags.map((String tag) {
+                                        return Container(
+                                          decoration: const BoxDecoration(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(5.0),
+                                            ),
+                                            color: Color(0xFE9E6E6),
+                                          ),
+                                          margin: const EdgeInsets.symmetric(
+                                              horizontal: 5.0),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10.0, vertical: 10.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              InkWell(
+                                                child: Text(
+                                                  tag,
+                                                  style: const TextStyle(color: Colors.white),
+                                                ),
+                                                onTap: () { },
+                                              ),
+                                              const SizedBox(width: 4.0),
+                                              InkWell(
+                                                child: const Icon(
+                                                  Icons.close,
+                                                  size: 14.0,
+                                                  color: Colors.white,
+                                                ),
+                                                onTap: () {
+                                                  inputFieldValues
+                                                      .onTagRemoved(tag);
+                                                },
+                                              )
+                                            ],
+                                          ),
+                                        );
+                                      }).toList()),
+                                ),
+                              )
+                                  : null,
+                            ),
+                            onChanged: inputFieldValues.onTagChanged,
+                            onSubmitted: inputFieldValues.onTagSubmitted,
+                          ),
+                        );
+                      },
                     )
+                  ],
                 ),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 100),
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 30,),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Text("Tell everyone about yourself", style: TextStyle(fontSize: 16, color: Color(0xFFFFE2BE)),)),
-                      const SizedBox(height: 10,),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text("What interest you?", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),),
-                      ),
-                      const SizedBox(height: 30,),
-                      TextFieldTags<String>(
-                        textfieldTagsController: _stringTagController,
-                        initialTags: controller.localProfile.value.getDefault().interests,
-                        textSeparators: const [' ', ','],
-                        letterCase: LetterCase.normal,
-                        validator: (String tag) {
-                          if (_stringTagController.getTags!.contains(tag)) {
-                            return 'You\'ve already entered that';
-                          }
-                          return null;
-                        },
-                        inputFieldBuilder: (context, inputFieldValues) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 15),
-                            child: TextField(
-                              onTap: () {
-                                _stringTagController.getFocusNode?.requestFocus();
-                              },
-                              controller: inputFieldValues.textEditingController,
-                              focusNode: inputFieldValues.focusNode,
-                              decoration: InputDecoration(
-                                isDense: true,
-                                border: const OutlineInputBorder(
-                                    borderRadius: BorderRadius.all(Radius.circular(7))
-                                ),
-                                focusedBorder: const OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(7)),
-                                  borderSide: BorderSide(
-                                    color: Color(0xFE9E6E6)
-                                  )
-                                ),
-                                errorText: inputFieldValues.error,
-                                prefixIconConstraints:
-                                BoxConstraints(maxWidth: _distanceToField * 0.8),
-                                prefixIcon: inputFieldValues.tags.isNotEmpty
-                                    ? SingleChildScrollView(
-                                  controller: inputFieldValues.tagScrollController,
-                                  scrollDirection: Axis.vertical,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10),
-                                    child: Wrap(
-                                        runSpacing: 4.0,
-                                        spacing: 4.0,
-                                        children:
-                                        inputFieldValues.tags.map((String tag) {
-                                          return Container(
-                                            decoration: const BoxDecoration(
-                                              borderRadius: BorderRadius.all(
-                                                Radius.circular(5.0),
-                                              ),
-                                              color: Color(0xFE9E6E6),
-                                            ),
-                                            margin: const EdgeInsets.symmetric(
-                                                horizontal: 5.0),
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 10.0, vertical: 10.0),
-                                            child: Row(
-                                              mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                InkWell(
-                                                  child: Text(
-                                                    tag,
-                                                    style: const TextStyle(color: Colors.white),
-                                                  ),
-                                                  onTap: () { },
-                                                ),
-                                                const SizedBox(width: 4.0),
-                                                InkWell(
-                                                  child: const Icon(
-                                                    Icons.close,
-                                                    size: 14.0,
-                                                    color: Colors.white,
-                                                  ),
-                                                  onTap: () {
-                                                    inputFieldValues
-                                                        .onTagRemoved(tag);
-                                                  },
-                                                )
-                                              ],
-                                            ),
-                                          );
-                                        }).toList()),
-                                  ),
-                                )
-                                    : null,
-                              ),
-                              onChanged: inputFieldValues.onTagChanged,
-                              onSubmitted: inputFieldValues.onTagSubmitted,
-                            ),
-                          );
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
